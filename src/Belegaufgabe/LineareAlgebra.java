fe445e0f37c498c3a6dbab90ff70a6264fdc3934
rossy95 <d.rossburg@googlemail.com>
2016-04-29 13:56:52 +0200

    Update LineareAlgebra.java
    fixes

--- src/Belegaufgabe/LineareAlgebra.java
+++ src/Belegaufgabe/LineareAlgebra.java
@@ -89,13 +89,14 @@ public class LineareAlgebra {
         return Math.abs(v1.x - v2.x) + Math.abs(v1.y - v2.y)+ Math.abs(v1.z - v2.z);
     }
 
-    public static double[] crossProduct(double[] a, double[] b) {   //neumachen
-        double[] cross = new double[3];
-        cross[0] = a[1] * b[2] - a[2] * b[1];
-        cross[1] = a[2] * b[0] - a[0] * b[2];
-        cross[2] = a[0] * b[2] - a[1] * b[0];
-        return cross;
+    public static Vektor3D crossProduct(Vektor3D v1, Vektor3D v2) {  
+    	Vektor3D erg= new Vektor3D(0,0,0);
+        erg.x= v1.y*v2.z-v1.z*v2.y; 
+        erg.y= v1.z*v2.x-v1.x*v2.z; 
+        erg.z= v1.x*v2.y-v1.y*v2.x;
+        return erg;
     }
+    
     public static double dotProduct(Vektor2D v1, Vektor2D v2) { //Skalarprodukt
         return v1.x*v2.x+v1.y*v2.y;
     }
@@ -112,10 +113,10 @@ public class LineareAlgebra {
         return (dotProduct(v1,v2) /(length(v1)*length(v2)));
     }
 
-    /*public static double sinEquation(Vektor2D v1, Vektor2D v2) {  //Determinate?
-            return (determinante()/(length(v1)*length(v2)));
-    } */
-
+    public static double sinEquation(Vektor2D v1, Vektor2D v2) {  
+            return (determinante(v1,v2)/(length(v1)*length(v2)));
+    } 
+    
     public static double angleRad(Vektor2D v1, Vektor2D v2) {
         return degreeToRad(acos(cosEquation(v1,v2)));
     }
@@ -140,36 +141,13 @@ public class LineareAlgebra {
         return  ((2*Math.PI)/360*deg);
     }
 
-    public static double determinante(double[][] matrix, int n) {   //neu!
-    	double det = 0;
-        double ergAdd = 0;
-        double ergSub = 0;
-        double matElem = 1;
-        double matElem2 = -1;
-        int j = 2;
-        int k = 0;
-        for (int p = 0; p < n; p++) {
-            matElem = 1;
-            matElem = -1;
-            for (int i = 0; i < n; i++) {
-                matElem = matrix[k][i] * matElem; 
-                matElem2 = matrix[k][j] * matElem2;
-                j--;
-                if (j == -1)
-                {
-                    j = n-1;
-                }
-                k = (k + 1)%n;
-            }
-            ergAdd += matElem;
-            ergSub -= matElem2;
-            det = ergAdd + ergSub;
-            k++;
-            System.out.println(det);
-        }   
-        return det;
+    public static double determinante(Vektor2D v1, Vektor2D v2) {   
+    	return (v1.x*v2.y-v2.x*v1.y);
     }
-
+    public static double determinante(Vektor3D v1, Vektor3D v2, Vektor3D v3) {   //neu!
+        return (v1.x*v2.y*v3.z + v2.x*v3.y*v1.z + v3.x*v1.y*v2.z-
+        		v1.z*v2.y*v3.x - v2.z*v3.y*v1.x - v3.z*v1.y*v2.x);
+    }
     public static double abs(Vektor2D v1) {
     	return length(v1);
     }
@@ -190,14 +168,25 @@ public class LineareAlgebra {
     }
 
     public static void main(String args[]){
-        double vek[]={3,4,9};
-        double vek2[]={2,12,50};
+      
         Vektor2D a= new Vektor2D(2,3);
+        Vektor3D v0= new Vektor3D(-1,2,3);
+        Vektor3D v1= new Vektor3D(4,50,6);
+        Vektor3D v2= new Vektor3D(7,8,9);
+        Vektor2D v3= new Vektor2D(5,2);
+        Vektor2D v4= new Vektor2D(2,3);
         show(a);
         System.out.println(radToDegree(4.1));
       /* System.out.println(angleDegree(vek,vek2));
        System.out.println(angleRad(vek,vek2));
         System.out.println(radToDegree(0.6));
         System.out.println(degreeToRad(23));*/
+        show(crossProduct(v1,v2));
+        System.out.println(determinante(v3,v4));
+        System.out.println(determinante(v0,v1,v2));
+        System.out.println(length(v3));
+        System.out.println(length(v4));
+        System.out.println(sinEquation(v3,v4));
+        
     }
 }
