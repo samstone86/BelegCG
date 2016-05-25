450eba5872e20782364d6a43111c3b66de6873f5
Philipp Vogel <philippwolf86.pw@gmail.com>
2016-05-25 14:31:15 +0200

    Update Triangle.java

--- src/Schwarmverhalten/Triangle.java
+++ src/Schwarmverhalten/Triangle.java
@@ -1,28 +1,28 @@
 package Schwarmverhalten;
 
-import static org.lwjgl.opengl.GL11.GL_TRIANGLE_FAN;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
 import static org.lwjgl.opengl.GL11.glBegin;
 import static org.lwjgl.opengl.GL11.glColor3d;
 import static org.lwjgl.opengl.GL11.glEnd;
-import static org.lwjgl.opengl.GL11.glVertex2f;
+import static org.lwjgl.opengl.GL11.glVertex2d;
 import math.Vektor2D;
 
 public class Triangle extends BewegendesObjekt {
     private static int objCounter = 0;
-    private float radius;
+    private double d;
     private double r, g, b;
 
     public Triangle() {
-        this(new Vektor2D(0, 0), new Vektor2D(2, 0), 20.0f, 1, 1, 0);
+        this(new Vektor2D(0, 0), new Vektor2D(2, 0), 1, 1, 0);
     }
 
     public Triangle(Vektor2D position, Vektor2D velocity) {
-        this(position, velocity, 20.0f, 1, 1, 0);
+        this(position, velocity, 1, 1, 0);
     }
 
-    public Triangle(Vektor2D position, Vektor2D velocity, float radius, double r, double g, double b) {
+    public Triangle(Vektor2D position, Vektor2D velocity, double r, double g, double b) {
         super(position, velocity);
-        this.radius = radius;
+        this.d = d;
         this.r=r;
         this.g=g;
         this.b=b;
@@ -32,12 +32,10 @@ public class Triangle extends BewegendesObjekt {
     @Override
     public void render() {
         glColor3d(r, g, b);
-        glBegin(GL_TRIANGLE_FAN);
-        glVertex2f((float)position.x, (float)position.y);
-        for (int angle=0; angle<360; angle+=5) {
-            glVertex2f((float)position.x + (float)Math.sin(angle) * radius, (float)position.y + (float)Math.cos(angle) * radius);
-        }
+        glBegin(GL_TRIANGLES);
+	        glVertex2d(20 + position.x, 20 + position.y);
+	        glVertex2d(30 + position.x, 40 + position.y);
+	        glVertex2d(40 + position.x, 20 + position.y);
         glEnd();
     }
 }
-
